aff3cfc99b636503a961dcdc37829da72e169c66
ng-teamdunstan <nicolaj@teamdunstan.com>
2025-08-02 15:20:27 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,40 @@
+// next.config.js - JAVASCRIPT VERSION
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Keine experimentellen Features
-  // Bewährte, stabile Konfiguration
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.scdn.co',
+        port: '',
+        pathname: '/image/**',
+      },
+      {
+        protocol: 'https', 
+        hostname: 'mosaic.scdn.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lineup-images.scdn.co', 
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'wrapped-images.spotifycdn.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images-ak.spotifycdn.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
